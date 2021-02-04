14e5f3fe881defeb345e51560a6f43e3069ac463
Mario Thompson <MarioThompson0010@gmail.com>
2021-02-03 21:34:36 -0600

    deploying

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+// router.get("/", ({body}, res) => {
+//   Transaction.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(404).json(err);
+//     });
+
+// });
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
